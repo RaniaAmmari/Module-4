53087277c72e22aed21ad6eed147cb74d0672199
Rania Ammari <rammari@dom.tti>
2022-06-29 16:11:13 +0100

    activity updated

--- my_project_directory/src/Controller/BlogController.php
+++ my_project_directory/src/Controller/BlogController.php
@@ -25,4 +25,23 @@ class BlogController extends AbstractController
         return $this->render('blog/home.html.twig'); 
         
     }
-}
+     /**
+     * @Route("/blog/article{id}", name="blog-show" )
+     */
+   
+    public function show(int $id){
+        // if($id==1){
+          return $this->render('blog/show.html.twig',[
+            'id' => $id]);}
+        
+    //     elseif ($id==2){return $this->render('blog/show1.html.twig');
+   
+    //     }
+    //     elseif ($id==3){return $this->render('blog/show3.html.twig');
+   
+    //     }
+    //     elseif ($id>3){ throw $this->createNotFoundException('Article does not exist');
+    //         $this->render('blog/error.html.twig'); }
+          
+    //    }
+    }
